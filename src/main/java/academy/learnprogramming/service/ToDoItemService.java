44cf5d1193ffe9212d83b86bba54c488dd34c194
Aiste Eimontaite <eimontaite.aiste@gmail.com>
2020-06-04 15:48:38 +0300

    Implement methods in ToDoItemServiceImpl

--- src/main/java/academy/learnprogramming/service/ToDoItemService.java
+++ src/main/java/academy/learnprogramming/service/ToDoItemService.java
@@ -3,14 +3,16 @@ package academy.learnprogramming.service;
 import academy.learnprogramming.model.ToDoData;
 import academy.learnprogramming.model.ToDoItem;
 
+import java.util.List;
+
 public interface ToDoItemService {
 	void addItem(ToDoItem itemToCreate);
 
 	void removeItem(int id);
 
-	ToDoItem getItem();
+	ToDoItem getItem(int id);
 
 	void updateItem(ToDoItem itemToUpdate);
 
-	ToDoData getData();
+	List<ToDoItem> getData();
 }
--- src/main/java/academy/learnprogramming/service/ToDoItemServiceImpl.java
+++ src/main/java/academy/learnprogramming/service/ToDoItemServiceImpl.java
@@ -4,34 +4,36 @@ import academy.learnprogramming.model.ToDoData;
 import academy.learnprogramming.model.ToDoItem;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ToDoItemServiceImpl implements ToDoItemService {
 	// == fields ==
 	private final ToDoData toDoData = new ToDoData();
-	
+
 	// == public methods ==
 	@Override
 	public void addItem(ToDoItem itemToCreate) {
-
+		toDoData.addItem(itemToCreate);
 	}
 
 	@Override
 	public void removeItem(int id) {
-
+		toDoData.removeItem(id);
 	}
 
 	@Override
-	public ToDoItem getItem() {
-		return null;
+	public ToDoItem getItem(int id) {
+		return toDoData.getItem(id) ;
 	}
 
 	@Override
 	public void updateItem(ToDoItem itemToUpdate) {
-
+		toDoData.updateItem(itemToUpdate);
 	}
 
 	@Override
-	public ToDoData getData() {
-		return null;
+	public List<ToDoItem> getData() {
+		return toDoData.getItems();
 	}
 }
